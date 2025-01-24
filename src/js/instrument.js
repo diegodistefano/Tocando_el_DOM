3e07b48ec93264dfc57807c810a8d857c5b79ea9
diegodistefano <diego.distefano.1989@gmail.com>
2025-01-24 11:57:41 +0100

    JS al presionar un boton

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,1 +1,9 @@
 //Codigo de JS del Piano
+
+let boton = document.querySelector(".reproductor")
+
+    boton.addEventListener("click", () => {
+      let etiquetaAudio = document.createElement("audio")
+      etiquetaAudio.setAttribute("src", "ubicación de tu archivo de audio")
+      etiquetaAudio.play()
+    })
