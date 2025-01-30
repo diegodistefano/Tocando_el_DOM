785cf3912f82d4e8ee019e9d99065f0ff4b5e34c
diegodistefano <diego.distefano.1989@gmail.com>
2025-01-30 10:54:54 +0100

    todas las teclas con sonido

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,7 +1,7 @@
 //Codigo de JS del Piano
 let nota = document.querySelectorAll('.nota');
 
-let sonidosArray_src = ["../public/sound/Do.mp3", "../public/sound/Re.mp3", "../public/sound/Mi.mp3", "../public/sound/Fa.mp3", "../public/sound/Sol.mp3", "../public/sound/La.mp3", "../public/sound/Si.mp3"];
+let sonidosArray_src = ["../public/sound/DO4.mp3", "../public/sound/REb4.mp3", "../public/sound/RE4.mp3", "../public/sound/MIb4.mp3", "../public/sound/MI4.mp3", "../public/sound/FA4.mp3", "../public/sound/SOLb4.mp3", "../public/sound/SOL4.mp3", "../public/sound/LAb4.mp3", "../public/sound/LA4.mp3", "../public/sound/SIb4.mp3", "../public/sound/SI4.mp3", "../public/sound/DO5.mp3", "../public/sound/REb5.mp3", "../public/sound/RE5.mp3", "../public/sound/MIb5.mp3", "../public/sound/MI5.mp3"];
 
 for (let i = 0; i < nota.length; i++) {
     nota[i].addEventListener('click', (e) => {  
@@ -9,28 +9,3 @@ for (let i = 0; i < nota.length; i++) {
         sonido.play();
     });
 }
-
-
-//const notaArray = Array.from(nota);
-
-
-// const indice = notaArray.indexOf(e.target);
-// console.log('Nota ' + (indice + 1) + ' del array: ' + e.target.textContent);
-
-/*
-for(let i = 0; i<nota.length;i++){
-    nota[i].addEventListener('click', (e) => {  
-    console.log('Nota ' + notaArray.from(nota).indexOf(e.target) + ' del array');
-  });
-}
-
-let sonidos_src = "../public/sound/tecla-do.mp3";
-
-for(let i = 0; i<sonidosArray_src.length;i++){
-    sonidosArray_src[i].addEventListener('click', (e) => {  
-        return e.sonidosArray_src[i];
-    });
-}
-
-sonido.src = sonidosArray_src[i];
-*/
