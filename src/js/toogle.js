791210b985d0d9f72226ae23685fee1313c39f11
jomabal98 <josemanuelbarreiroalvarez@gmail.com>
2025-01-30 13:01:36 +0100

    dropdown nav

--- src/js/toogle.js
+++ src/js/toogle.js
@@ -9,3 +9,27 @@ buttonToggle.addEventListener('click', function () {
         menu.setAttribute('class', 'toogle');
     }
 })
+
+// nav dropdown
+let dropdown = menu.querySelector('.dropdown');
+let menuDropdown = document.querySelector('.dropdown-list');
+dropdown.addEventListener('click', function () {
+    if (window.innerWidth <= 768) {
+        return;
+    }
+    // let submenu = menu.querySelector('.submenu');
+    // if (submenu.style.display == 'none') {
+    //     submenu.style.display = 'block';
+    // } else {
+    //     submenu.style.display = 'none';
+    // }
+    if (dropdown.classList.replace('fa-caret-down', 'fa-caret-up')) {
+        menuDropdown.setAttribute('class', 'dropdown-list dropdown-list-active');
+    } else {
+        menuDropdown.setAttribute('class', 'dropdown-list');
+        dropdown.classList.replace('fa-caret-up', 'fa-caret-down')
+    }
+    // console.log(dropdown.classList.replace('fa-caret-down','fa-caret-up'));
+
+
+})
